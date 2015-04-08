45b5e24a706cd447806cf6668a6b98cc83cafbeb
Brice Culas <darkbreci@hotmail.fr>
2015-04-08 22:18:49 +0200

    no message

--- module2/module2.js
+++ module2/module2.js
@@ -1,21 +1,24 @@
-var gl = null,
-        canvas = null,
-        glProgram = null,
+var vertexShader = null,
         fragmentShader = null,
-        vertexShader = null;
+        shaderProgram = null;
+
+var canvas;
+var gl = null;
 
 var radius = 0.9;
 var angle = 10;
 
 function initWebGL()
 {
+    //don't touch this part
     canvas = document.getElementById("my-canvas");
-    try 
+    try
     {
         gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
-    } 
-    catch (e) 
-    {}
+    }
+    catch (e)
+    {
+    }
 
     if (gl)
     {
@@ -24,8 +27,8 @@ function initWebGL()
         initShaders(); //prepare shader
         setupBuffers(); // prepare data
         drawScene(); //draw data
-    } 
-    else 
+    }
+    else
     {
         alert("Error: Your browser does not appear to support WebGL.");
     }
@@ -38,39 +41,40 @@ function setupWebGL()
 
 function initShaders()
 {
-    var fs_source = document.getElementById('shader-fs').innerHTML,
-            vs_source = document.getElementById('shader-vs').innerHTML;
+    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 
-    vertexShader = makeShader(vs_source, gl.VERTEX_SHADER);
-    fragmentShader = makeShader(fs_source, gl.FRAGMENT_SHADER);
+    var vertex_source = document.getElementById('shader-vs').innerHTML;
+    var fragment_source = document.getElementById('shader-fs').innerHTML;
+
+    gl.shaderSource(vertexShader, vertex_source);
+    gl.shaderSource(fragmentShader, fragment_source);
+
+    gl.compileShader(vertexShader);
+    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+        alert("Error compiling shader: " + gl.getShaderInfoLog(vertexShader));
+    }
+
+    gl.compileShader(fragmentShader);
+    if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+        alert("Error compiling shader: " + gl.getShaderInfoLog(fragmentShader));
+    }
 
     //create program
-    glProgram = gl.createProgram();
+    shaderProgram = gl.createProgram();
 
-    gl.attachShader(glProgram, vertexShader);
-    gl.attachShader(glProgram, fragmentShader);
-    gl.linkProgram(glProgram);
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
 
-    if (!gl.getProgramParameter(glProgram, gl.LINK_STATUS)) {
+    gl.linkProgram(shaderProgram);
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
         alert("Unable to initialize the shader program.");
     }
 
-    //use program
-    gl.useProgram(glProgram);
+    gl.useProgram(shaderProgram);
+
 }
 
-function makeShader(src, type)
-{
-    //compile the vertex shader
-    var shader = gl.createShader(type);
-    gl.shaderSource(shader, src);
-    gl.compileShader(shader);
-
-    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        alert("Error compiling shader: " + gl.getShaderInfoLog(shader));
-    }
-    return shader;
-}
 
 function setupBuffers()
 {
@@ -88,9 +92,9 @@ function setupBuffers()
 
 function drawScene()
 {
-    var radiusLocation = gl.getUniformLocation(glProgram, "radius");
+    var radiusLocation = gl.getUniformLocation(shaderProgram, "radius");
     gl.uniform1f(radiusLocation, radius);
-    var angleLocation = gl.getAttribLocation(glProgram, "angle");
+    var angleLocation = gl.getAttribLocation(shaderProgram, "angle");
     gl.enableVertexAttribArray(angleLocation);
     gl.bindBuffer(gl.ARRAY_BUFFER, angleBuffer);
     gl.vertexAttribPointer(angleLocation, 1, gl.FLOAT, false, 0, 0);
